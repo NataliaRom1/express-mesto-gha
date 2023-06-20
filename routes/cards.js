2146563065b4ccf13e1d6fb88759a379ab118257
Nata <Natalia.Romanovskaia@skoltech.ru>
2023-06-20 15:52:58 +0300

    Спринт 13-1

--- routes/cards.js
+++ routes/cards.js
@@ -11,6 +11,6 @@ router.get('/cards', getCards);
 router.post('/cards', createCard);
 router.delete('/cards/:cardId', deleteCardById);
 router.patch('/cards/:cardId/likes', addCardLike);
-router.patch('/cards/:cardId/like', deleteCardLike);
+router.patch('/cards/:cardId/likes', deleteCardLike);
 
 module.exports = router;
